9a811e68847ec17b0206727e51b70cfe33d51248
Aakash Singh <aakash.singh@w3villa>
2026-03-24 18:05:41 +0530

    ee

--- src/main/java/com/w3villa/mini_project_backend/entites/User.java
+++ src/main/java/com/w3villa/mini_project_backend/entites/User.java
@@ -39,7 +39,9 @@ public class User implements UserDetails {
     private  Provider provider = Provider.LOCAL;
 
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     @PrePersist
