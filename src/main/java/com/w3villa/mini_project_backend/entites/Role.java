5e95e86d4705ceb7c6712e7e9bbe28ff7ba12102
Aakash Singh <aakash.singh@w3villa>
2026-03-24 18:05:41 +0530

    ee

--- src/main/java/com/w3villa/mini_project_backend/entites/Role.java
+++ src/main/java/com/w3villa/mini_project_backend/entites/Role.java
@@ -3,19 +3,20 @@ package com.w3villa.mini_project_backend.entites;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 import java.util.UUID;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
-@Entity(name="roles")
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
 public class Role {
     @Id
-    @Column(name="role_id")
-    private UUID id= UUID.randomUUID();
-    @Column(unique = true,nullable = false)
+    private UUID id = UUID.randomUUID();
+    @Column(unique = true, nullable = false)
     private String name;
 }
