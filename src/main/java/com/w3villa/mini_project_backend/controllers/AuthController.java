6f11697adad0d53c4940127c4906cf5b415125de
Aakash Singh <aakash.singh@w3villa>
2026-03-25 14:56:55 +0530

    Completed logout refreshtoken

--- src/main/java/com/w3villa/mini_project_backend/controllers/AuthController.java
+++ src/main/java/com/w3villa/mini_project_backend/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.w3villa.mini_project_backend.controllers;
 
 import com.w3villa.mini_project_backend.dtos.LoginRequest;
+import com.w3villa.mini_project_backend.dtos.RefreshTokenRequest;
 import com.w3villa.mini_project_backend.dtos.TokenResponse;
 import com.w3villa.mini_project_backend.dtos.UserDto;
 import com.w3villa.mini_project_backend.entites.RefreshToken;
@@ -10,15 +11,20 @@ import com.w3villa.mini_project_backend.repositories.UserRepository;
 import com.w3villa.mini_project_backend.security.CookieService;
 import com.w3villa.mini_project_backend.security.JWTService;
 import com.w3villa.mini_project_backend.services.AuthService;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.Authentication;
 
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -80,6 +88,139 @@ public class AuthController {
 
     }
 
+    @PostMapping("/refresh")
+    public ResponseEntity<TokenResponse> refreshToken(
+            @RequestBody(required = false) RefreshTokenRequest body,
+            HttpServletResponse response,
+            HttpServletRequest request
+    ) throws InterruptedException {
+
+
+        //Thread.sleep(5000);
+
+        String refreshToken = readRefreshTokenFromRequest(body, request).orElseThrow(() -> new BadCredentialsException("Refresh token is missing"));
+
+
+        if(!jwtService.isRefreshToken(refreshToken)){
+            throw new BadCredentialsException("Invalid Refresh Token Type");
+        }
+
+        String jti = jwtService.getJti(refreshToken);
+        UUID userId = jwtService.getUserId(refreshToken);
+        RefreshToken storedRefreshToken = refreshTokenRepository.findByJti(jti).orElseThrow(() -> new BadCredentialsException("Refresh token not recognized"));
+
+        if(storedRefreshToken.isRevoked()){
+            throw new BadCredentialsException("Refresh token expired or revoked");
+        }
+
+        if(storedRefreshToken.getExpiresAt().isBefore(Instant.now())){
+            throw new BadCredentialsException("Refresh token expired");
+        }
+
+        if(!storedRefreshToken.getUser().getId().equals(userId)){
+            throw new BadCredentialsException("Refresh token does not belong to this user");
+        }
+
+        //refresh token ko rotate:
+        storedRefreshToken.setRevoked(true);
+        String newJti= UUID.randomUUID().toString();
+        storedRefreshToken.setReplacedByToken(newJti);
+        refreshTokenRepository.save(storedRefreshToken);
+
+        User user = storedRefreshToken.getUser();
+
+        var newRefreshTokenOb = RefreshToken.builder()
+                .jti(newJti)
+                .user(user)
+                .createdAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(jwtService.getRefreshTtlSeconds()))
+                .revoked(false)
+                .build();
+
+        refreshTokenRepository.save(newRefreshTokenOb);
+        String newAccessToken= jwtService.generateAccessToken(user);
+        String newRefreshToken = jwtService.generateRefreshToken(user, newRefreshTokenOb.getJti());
+
+
+        cookieService.attachRefreshCookie(response, newRefreshToken, (int) jwtService.getRefreshTtlSeconds());
+        cookieService.addNoStoreHeaders(response);
+        return ResponseEntity.ok(TokenResponse.of(newAccessToken, newRefreshToken, jwtService.getAccessTtlSeconds(), mapper.map(user, UserDto.class)));
+
+
+
+
+
+
+    }
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
+        readRefreshTokenFromRequest(null, request).ifPresent(token -> {
+            try {
+                if (jwtService.isRefreshToken(token)) {
+                    String jti = jwtService.getJti(token);
+                    refreshTokenRepository.findByJti(jti).ifPresent(rt -> {
+                        rt.setRevoked(true);
+                        refreshTokenRepository.save(rt);
+                    });
+                }
+            } catch (JwtException ignored) {
+            }
+        });
+
+        // Use CookieUtil (same behavior)
+        cookieService.clearRefreshCookie(response);
+        cookieService.addNoStoreHeaders(response);
+        SecurityContextHolder.clearContext();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    //this method will read refresh token from request header or body.
+    private Optional<String> readRefreshTokenFromRequest(RefreshTokenRequest body, HttpServletRequest request) {
+//            1. prefer reading refresh token from cookie
+        if (request.getCookies() != null) {
+
+            Optional<String> fromCookie = Arrays.stream(request.getCookies())
+                    .filter(c -> cookieService.getRefreshTokenCookieName().equals(c.getName()))
+                    .map(Cookie::getValue)
+                    .filter(v -> !v.isBlank())
+                    .findFirst();
+
+            if (fromCookie.isPresent()) {
+                return fromCookie;
+            }
+
+
+        }
+
+        // 2 body:
+        if (body != null && body.refreshToken() != null && !body.refreshToken().isBlank()) {
+            return Optional.of(body.refreshToken());
+        }
+
+        //3. custom header
+        String refreshHeader = request.getHeader("X-Refresh-Token");
+        if (refreshHeader != null && !refreshHeader.isBlank()) {
+            return Optional.of(refreshHeader.trim());
+        }
+
+        //Authorization = Bearer <token>
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader != null && authHeader.regionMatches(true, 0, "Bearer ", 0, 7)) {
+            String candidate = authHeader.substring(7).trim();
+            if (!candidate.isEmpty()) {
+                try {
+                    if (jwtService.isRefreshToken(candidate)) {
+                        return Optional.of(candidate);
+                    }
+                } catch (Exception ignored) {
+                }
+            }
+        }
+
+        return Optional.empty();
+
+
+    }
 
     private Authentication authenticate(LoginRequest loginRequest) {
         try {
