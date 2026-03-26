35d943b7b95bf32e9a8d9199b559156a76098f0e
Aakash Singh <aakash.singh@w3villa>
2026-03-26 17:55:21 +0530

    Cors config

---
+++ src/main/java/com/w3villa/mini_project_backend/config/AppConstants.java
@@ -0,0 +1,27 @@
+package com.w3villa.mini_project_backend.config;
+
+public class AppConstants {
+
+
+    public static final String[] AUTH_PUBLIC_URLS = {
+            "/api/v1/auth/**",
+            "/v3/api-docs/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/api/v1/users/**"
+    };
+
+    public static final String[] AUTH_ADMIN_URLS= {
+//            "/api/v1/users/**"
+    };
+
+    public static final String[] AUTH_GUEST_URLS= {
+
+    };
+
+    public static final String ADMIN_ROLE = "ADMIN";
+    public static final String GUEST_ROLE = "GUEST";
+
+
+
+}
